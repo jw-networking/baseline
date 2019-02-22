6370e5b8ddc7d66d18b5a7d7757549310f84f5c7
JoshWalker <me@joshwalker.tech>
2019-02-22 08:11:19 +0000

    basic flow created for mesos

--- bench_mesos.py
+++ bench_mesos.py
@@ -1,8 +1,45 @@
 #!/bin/python
 import requests
 import json
+import sys
 
+#vars
+resultsPath="./results"
+jsonPath="./mesos"
+containerCounts=[5,10,20]
+iterations=100
+apiURI="http://master1.mesos/v2/apps"
+
+##############################
+#functions
+##############################
+def fillTo(count):
+	print("fill to "+str(count))
+
+def batchRun(runs):
+	print("batch run")
+	return "batchrun"
+
+def batchList(runs):
+	print("batch list")
+	return "batchList"
+
+def cleanup():
+	fillTo(0)
 
 ##############################
 #main test loop
 ##############################
+
+for count in containerCounts:
+	fillTo(count)
+
+	print("Starting run test")
+	results=open(resultsPath+"/mesos-run-"+str(count)+".raw","w")
+	results.write(batchRun(iterations))
+
+	print("Starting list test")
+	results=open(resultsPath+"/mesos-list-"+str(count)+".raw","w")
+	results.write(batchList(iterations))
+
+cleanup()
