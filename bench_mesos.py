e23ce9c2de03a68242e55b0e73b2a9740c458a18
root <root@master1.mesos>
2019-02-22 11:57:40 +0000

    cleanup

--- bench_mesos.py
+++ bench_mesos.py
@@ -2,19 +2,24 @@
 import requests
 import json
 import sys
+import os
 
+##############################
 #vars
+##############################
 resultsPath="./results"
 jsonPath="./mesos"
 containerCounts=[5,10,20]
-iterations=100
+iterations=10
 apiURI="http://master1.mesos/v2/apps"
+scaleID="scale"
+tempID="temp"
 
 ##############################
 #functions
 ##############################
-def fillTo(count):
-	print("fill to "+str(count))
+def fillTo(appID,count):
+	print("fill "+appID+" to "+str(count))
 
 def batchRun(runs):
 	print("batch run")
@@ -24,15 +29,21 @@ def batchList(runs):
 	print("batch list")
 	return "batchList"
 
-def cleanup():
-	fillTo(0)
+
+##############################
+#prep
+##############################
+
+if(not os.path.isdir(resultsPath)):
+	os.mkdir(resultsPath)
+
 
 ##############################
 #main test loop
 ##############################
 
 for count in containerCounts:
-	fillTo(count)
+	fillTo(scaleID,count)
 
 	print("Starting run test")
 	results=open(resultsPath+"/mesos-run-"+str(count)+".raw","w")
@@ -42,4 +53,7 @@ for count in containerCounts:
 	results=open(resultsPath+"/mesos-list-"+str(count)+".raw","w")
 	results.write(batchList(iterations))
 
-cleanup()
+##############################
+#cleanup
+##############################
+
