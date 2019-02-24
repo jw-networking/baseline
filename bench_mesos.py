8bb20257d60c96c74f63fbd568a962969f32f368
JoshWalker <me@joshwalker.tech>
2019-02-24 14:04:10 +0000

    update json and service creation

--- bench_mesos.py
+++ bench_mesos.py
@@ -4,6 +4,7 @@ import json
 import sys
 import os
 import re
+import socket
 
 ##############################
 #data types
@@ -25,17 +26,41 @@ jsonPath="./mesos"
 containerCounts=[5,10,20]
 iterations=10
 apiURI="http://master1.mesos:8080/v2/apps"
-scale=service("scale",open(jsonPath+"/scale.json").read().replace("\n",""))
-ncBack=service("ncBack",open(jsonPath+"/ncBack.json").read().replace("\n",""))
+ncPort=4444
+scale=None
+ncBack=None
 
 ##############################
 #functions
 ##############################
+
+##
+#services
+def getIP():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	IP=s.getsockname()[0]
+	s.close()
+	
+	return IP
+
+def httpCheck(cmd,destination,json=None):
+	response=cmd(destination,data=json)
+	status=response.status_code
+	if status<200 or status>300:
+		print(response)
+		exit(status)
+	return response
+	
+
+##
+#main loop
+
 def fillTo(app,count):
 	print("fill "+app.appID+" to "+str(count))
 
-def batchRun(runs,svc):
-	print("batch run",svc)
+def batchRun(runs):
+	print("batch run")
 	return "batchrun"
 
 def batchList(runs):
@@ -44,10 +69,11 @@ def batchList(runs):
 
 def deployService(svc):
 	print("deploying",svc)
-	
+	httpCheck(requests.post,apiURI,svc.manifest)
 
 def destroyService(svc):
 	print("destroying",svc)
+
 
 ##############################
 #prep
@@ -55,6 +81,13 @@ def destroyService(svc):
 
 if(not os.path.isdir(resultsPath)):
 	os.mkdir(resultsPath)
+
+ncBackJSON=json.loads(open(jsonPath+"/ncBack.json").read())
+ncBackJSON["cmd"]='echo ""| nc '+getIP()+" "+str(ncPort)
+scaleJSON=json.loads(open(jsonPath+"/scale.json").read())
+
+scale=service("scale",json.dumps(scaleJSON))
+ncBack=service("ncBack",json.dumps(ncBackJSON))
 
 deployService(scale)
 
@@ -67,7 +100,7 @@ for count in containerCounts:
 
 	print("Starting run test")
 	results=open(resultsPath+"/mesos-run-"+str(count)+".raw","w")
-	results.write(batchRun(iterations,ncBack))
+	results.write(batchRun(iterations))
 
 	print("Starting list test")
 	results=open(resultsPath+"/mesos-list-"+str(count)+".raw","w")
